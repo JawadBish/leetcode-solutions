f2cf7941737d8bd90abc0cf18ecd6d9f40e0ac8f
Jawad Bisharat <jawad.bisharat@amdocs.com>
2024-07-13 20:09:29 +0300

    leetcode09

---
+++ leetcode/src/main/java/easy/Leetcode09.java
@@ -0,0 +1,19 @@
+package easy;
+
+public  class Leetcode09 {
+        public static boolean isPalindrome(int x) {
+            if ( x < 0 ) return false;
+            int reversed = 0;
+            int temp = x;
+            while (temp != 0) {
+                reversed = reversed * 10 + temp % 10;
+                temp = temp /10 ;
+            }
+            return (reversed == x);
+        }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1114));
+    }
+}
