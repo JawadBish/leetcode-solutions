8528173bce73a441d1a4125734491780ef26f36f
jawadb <jawad.bish@gmail.com>
2024-08-28 19:59:02 +0300

    merge solution leetcode top 150 interview questions

--- leetcode/src/main/java/interviews150/Merge88.java
+++ leetcode/src/main/java/interviews150/Merge88.java
@@ -15,15 +15,5 @@ public class Merge88 {
     }
 
     public static void main(String[] args) {
-        /*
-        int[] nums1 = new int[]{1,2,3,0,0,0};
-        int[] nums2 = new int[]{2,5,6};
-        m = 3     n = 3     pMerge = 5
-*/
-        for ( int i = 0 ; i < nums2.length ; i++) {
-            System.out.println(nums2[i]);
-        }
-
-         */
     }
 }
